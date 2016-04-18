7c707af37eadcf0079deef359afc743102da1488
Conor <conormurphy24@gmail.com>
2016-04-18 22:17:33 +0100

    Testing with widgets 1

--- Assignment3.py
+++ Assignment3.py
@@ -8,7 +8,13 @@ class calendarApp(tkinter.Tk):
 
 
     def initialise(self):
-        pass
+        self.grid()
+
+        self.entry = tkinter.Entry(self)
+        self.entry.grid(column=0,row=0,sticky="EW")
+
+        button = tkinter.Button(self, text=u'Test Button')
+        button.grid(column=1,row=1)
 
 
 if __name__ == '__main__':
