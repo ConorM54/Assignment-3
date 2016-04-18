dc283e7d2f9f440ff8d2b307c6b0ebcf1d8187ee
Conor <conormurphy24@gmail.com>
2016-04-18 23:24:53 +0100

    Testing with widgets 2

--- Assignment3.py
+++ Assignment3.py
@@ -9,12 +9,33 @@ class calendarApp(tkinter.Tk):
 
     def initialise(self):
         self.grid()
-
-        self.entry = tkinter.Entry(self)
+        self.entryVariable = tkinter.StringVar()
+        self.entry = tkinter.Entry(self, textvariable = self.entryVariable)
         self.entry.grid(column=0,row=0,sticky="EW")
+        self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u"Enter text here")
 
-        button = tkinter.Button(self, text=u'Test Button')
+        button = tkinter.Button(self, text=u'Test Button', command = self.OnButtonClick)
         button.grid(column=1,row=1)
+
+        self.labelVariable = tkinter.StringVar()
+        label = tkinter.Label(self, textvariable=self.labelVariable,anchor="w",fg="white",bg = "blue")
+        label.grid(column=0, row =1, rowspan=2, sticky = "EW")
+        self.labelVariable.set(u"Hello !")
+
+
+        #Allows user to resize window
+        self.grid_columnconfigure(0,weight=1)
+        #stops vertical resizing
+        self.resizable(True,False)
+        self.update()
+        self.geometry(self.geometry())
+
+    def OnButtonClick(self):
+       self.labelVariable.set(self.entryVariable.get())
+
+    def OnPressEnter(self,event):
+        self.labelVariable.set(self.entryVariable.get())
 
 
 if __name__ == '__main__':
