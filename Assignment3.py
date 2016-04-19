722975140115df3eb29b135ceb3c647a5313672a
Conor <conormurphy24@gmail.com>
2016-04-19 16:35:30 +0100

    Added label to display calendar

--- Assignment3.py
+++ Assignment3.py
@@ -9,7 +9,7 @@ class calendarApp(tkinter.Tk):
 
 
     def initialise(self):
-        self.grid()
+        
         self.labelVariable = tkinter.StringVar()
         label = tkinter.Label(self, textvariable= self.labelVariable,anchor="w",fg="white",bg = "gray")
         label.grid(column= 0, row = 0, sticky = "WS", padx= 2)
@@ -21,7 +21,7 @@ class calendarApp(tkinter.Tk):
         for month in range(1,13):
             monthList.append(calendar.month_name[month])
         self.monthsOpt = tkinter.OptionMenu(self, self.monthVal, *monthList)
-        self.monthsOpt.grid(column= 1, row = 0, sticky = "WS", padx= 2)
+        self.monthsOpt.grid(column= 1, row = 0, sticky = "EWS", padx= 2)
 
         self.labelVariable = tkinter.StringVar()
         label = tkinter.Label(self, textvariable= self.labelVariable,anchor="w",fg="white",bg = "gray")
@@ -34,7 +34,14 @@ class calendarApp(tkinter.Tk):
             yearList.append(year)
             
         self.yearOpt = tkinter.OptionMenu(self, self.yearVal, *yearList)
-        self.yearOpt.grid(column= 1, row = 1, sticky = "WS", padx= 2)
+        self.yearOpt.grid(column= 1, row = 1, sticky = "EWS", padx= 2)
+
+        self.cal = calendar.TextCalendar(calendar.MONDAY)
+        c = self.cal.formatmonth(2015, 1)
+        displayCalendar = tkinter.Label(self, textvariable= c,fg="white",bg = "gray")
+        displayCalendar.grid(column = 3, row = 0, rowspan= 3, sticky = "EW")
+
+
         """self.labelVariable = tkinter.StringVar()
         label = tkinter.Label(self, textvariable=self.labelVariable,anchor="w",fg="white",bg = "blue")
         label.grid(column=0, row =1, rowspan=2, sticky = "SEW")
@@ -43,11 +50,13 @@ class calendarApp(tkinter.Tk):
 
         #Allows user to resize window
         #self.grid_columnconfigure(0,weight=1)
-        #self.grid_columnconfigure(1,weight=3)
+        #self.grid_columnconfigure(1,weight=1)
+        #self.grid_columnconfigure(2,weight=1)
         #stops vertical resizing
         self.resizable(True,False)
         self.update()
         self.geometry(self.geometry())
+        self.grid()
 
     def OnButtonClick(self):
        self.labelVariable.set(self.entryVariable.get())
@@ -59,6 +68,7 @@ class calendarApp(tkinter.Tk):
 if __name__ == '__main__':
     app = calendarApp(None)
     app.title ('Calendar App')
+    app.wm_geometry("400x200")
     app.mainloop()
 
         
---
+++ test.py
@@ -0,0 +1,4 @@
+import calendar
+cal = calendar.TextCalendar(calendar.MONDAY)
+c = cal.formatmonth(2015, 1)
+print (c)
